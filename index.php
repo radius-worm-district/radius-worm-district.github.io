418c80f2c37bd4ef0856ffc714e50dba0eeef090
Tomasz Tabis <tomaszt@hotmail.nl>
2017-03-14 09:18:10 +0100

    Added HTML Boilerplate
    Added HTML Boilerplate for better optimalisation and support of html and
    webbrowsers

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+    //require classes 
+?>
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>De Gokkers</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="apple-touch-icon" href="apple-touch-icon.png">
+        <!-- Place favicon.ico in the root directory -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/main.css">
+        <script src="js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <!-- Add your site or application content here -->
+        
+        <header>
+            <!-- Header / Logo / Login -->
+            <div class="wrapper">
+
+            </div>
+        </header>
+        <video>
+            <!-- Promo Video -->
+            <div class="wrapper">
+                
+            </div>
+        </video>
+        <div class="about">
+            <!-- About Game -->
+            <div class="wrapper">
+
+            </div>
+        </div>
+        <div class="register">
+            <!-- Register User -->
+            <div class="wrapper">
+                
+            </div>
+        </div>
+        <div class="download">
+            <!-- Download Button -->
+            <div class="wrapper">
+                
+            </div>
+        </div>
+        <footer>
+            <!-- Footer / Credits / Privacy Policy -->
+            <div class="wrapper">
+                
+            </div>
+        </footer>
+    </body>
+</html>
