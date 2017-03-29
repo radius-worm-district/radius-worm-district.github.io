9261dda2ba475aeb740235c610e671c84a3e5556
Titson <tomaszt@hotmail.nl>
2017-03-29 19:44:03 +0200

    Changed font and font size

--- index.php
+++ index.php
@@ -14,6 +14,8 @@
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
         <!-- Place favicon.ico in the root directory -->
 
+        <link href="https://fonts.googleapis.com/css?family=Orbitron|Revalia|Roboto+Mono" rel="stylesheet">
+
         <link rel="stylesheet" href="css/normalize.css">
         <link rel="stylesheet" href="css/main.css">
         <script src="js/vendor/modernizr-2.8.3.min.js"></script>
